ddc5150ad780be15a09dca92e45f3b50bd13f1f1
Divine <divinetidy@gmail.com>
2024-12-20 02:25:40 +0100

    updated

--- src/components/hero/Animation.jsx
+++ src/components/hero/Animation.jsx
@@ -1,27 +1,157 @@
-import roller1 from "../../assets/icons/roller1.svg"
-import roller2 from "../../assets/icons/roller2.svg"
-import roller3 from "../../assets/icons/roller3.svg"
-import roller4 from "../../assets/icons/roller4.svg"
-import roller5 from "../../assets/icons/roller5.svg"
+import roller1 from "../../assets/icons/roller1.svg";
+import roller2 from "../../assets/icons/roller2.svg";
+import roller3 from "../../assets/icons/roller3.svg";
+import roller4 from "../../assets/icons/roller4.svg";
+import roller5 from "../../assets/icons/roller5.svg";
+import roller6 from "../../assets/icons/roller6.svg";
+import roller7 from "../../assets/icons/roller7.svg";
+import roller8 from "../../assets/icons/roller8.svg";
+import roller9 from "../../assets/icons/roller9.svg";
+import roller10 from "../../assets/icons/roller10.svg";
+import roller11 from "../../assets/icons/roller11.svg";
+import roller12 from "../../assets/icons/roller12.svg";
+import roller13 from "../../assets/icons/roller13.svg";
+import roller14 from "../../assets/icons/roller14.svg";
+import roller15 from "../../assets/icons/roller15.svg";
+import roller16 from "../../assets/icons/roller16.svg";
+import roller17 from "../../assets/icons/roller17.svg";
+import roller18 from "../../assets/icons/roller18.svg";
+import roller19 from "../../assets/icons/roller19.svg";
+import roller20 from "../../assets/icons/roller20.svg";
+import roller21 from "../../assets/icons/roller21.svg";
+import roller22 from "../../assets/icons/roller22.svg";
+import roller23 from "../../assets/icons/roller23.svg";
+import roller24 from "../../assets/icons/roller24.svg";
+import roller25 from "../../assets/icons/roller25.svg";
 
-
-
+const icons = [
+  {
+    id: 1,
+    src: roller1,
+  },
+  {
+    id: 2,
+    src: roller2,
+  },
+  {
+    id: 3,
+    src: roller3,
+  },
+  {
+    id: 4,
+    src: roller4,
+  },
+  {
+    id: 5,
+    src: roller5,
+  },
+  {
+    id: 6,
+    src: roller6,
+  },
+  {
+    id: 7,
+    src: roller7,
+  },
+  {
+    id: 8,
+    src: roller8,
+  },
+  {
+    id: 9,
+    src: roller9,
+  },
+  {
+    id: 10,
+    src: roller10,
+  },
+  {
+    id: 11,
+    src: roller11,
+  },
+  {
+    id: 12,
+    src: roller12,
+  },
+  {
+    id: 13,
+    src: roller13,
+  },
+  {
+    id: 14,
+    src: roller14,
+  },
+  {
+    id: 15,
+    src: roller15,
+  },
+  {
+    id: 16,
+    src: roller16,
+  },
+  {
+    id: 17,
+    src: roller17,
+  },
+  {
+    id: 18,
+    src: roller18,
+  },
+  {
+    id: 19,
+    src: roller19,
+  },
+  {
+    id: 20,
+    src: roller20,
+  },
+  {
+    id: 21,
+    src: roller21,
+  },
+  {
+    id: 22,
+    src: roller22,
+  },
+  {
+    id: 23,
+    src: roller23,
+  },
+  {
+    id: 24,
+    src: roller24,
+  },
+  {
+    id: 25,
+    src: roller25,
+  },
+];
 
 const Animation = () => {
   return (
     <div className="w-full h-[149px] overflow-hidden flex flex-col gap-[25px] ">
-    <div className="w-auto h-[62px] flex gap-[16px]  ">
-      <span className="w-[67px] h-[62px] px-[15px] py-[14px] bg-semiBlack rounded-lg border-[1px] border-white border-solid"><img src={roller1}/></span>
-      <span className="w-[67px] h-[62px] px-[15px] py-[14px] bg-semiBlack rounded-lg border-[1px] border-white border-solid"><img src={roller2}/></span>
-      <span className="w-[67px] h-[62px] px-[15px] py-[14px] bg-semiBlack rounded-lg border-[1px] border-white border-solid"><img src={roller3}/></span>
-      <span className="w-[67px] h-[62px] px-[15px] py-[14px] bg-semiBlack rounded-lg border-[1px] border-white border-solid"><img src={roller4}/></span>
-      <span className="w-[67px] h-[62px] px-[15px] py-[14px] bg-semiBlack rounded-lg border-[1px] border-white border-solid"><img src={roller5}/></span>
-
+      <div className="w-[2759px] h-[62px] flex gap-[16px]  ">
+        {icons.map((items) => (
+          <span
+            key={items.id}
+            className="w-[67px] h-[62px] px-[15px] py-[14px] bg-semiBlack rounded-lg border-[1px] border-white border-solid"
+          >
+            <img src={items.src} />
+          </span>
+        ))}
+      </div>
+      <div className="w-[2759px] h-[62px] flex gap-[16px]  ">
+        {icons.map((items) => (
+          <span
+            key={items.id}
+            className="w-[67px] h-[62px] px-[15px] py-[14px] bg-semiBlack rounded-lg border-[1px] border-white border-solid"
+          >
+            <img src={items.src} />
+          </span>
+        ))}
+      </div>
     </div>
-    <div className="w-auto h-[62px] flex gap-[16px]  border-solid border-[1px] border-white"></div>
+  );
+};
 
-  </div>
-  )
-}
-
-export default Animation
+export default Animation;
